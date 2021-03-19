33a6aa46be2b728795a37205dba5d13c260a3ebb
hayamizu_shinji <hayamizu.hnut@gmail.com>
2021-03-19 13:05:44 +0900

    add

---
+++ Downloads/AndroidSample/app/src/main/java/com/example/androidsample/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.androidsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.URLUtil;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
