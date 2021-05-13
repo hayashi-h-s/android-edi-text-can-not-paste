bbaf9fb634fb335943d95f87a80c427bd39af37e
hayamizu_shinji <hayamizu.hnut@gmail.com>
2021-05-13 09:01:08 +0900

    Listにポップアップメニュー

--- myapps/flutter/flutter_todo_app_firebase/lib/main.dart
+++ myapps/flutter/flutter_todo_app_firebase/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter_todo_app_firebase/add/add_page.dart';
 import 'package:flutter_todo_app_firebase/main_model.dart';
 import 'package:provider/provider.dart';
 
+import 'add/add_model.dart';
+
 void main() async {
   // これを呼ばないとFireStoreを呼び出せない
   WidgetsFlutterBinding.ensureInitialized(); // ← わからん
@@ -23,6 +25,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+//
+// class MyApp extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() {
+//     return _State();
+//   }
+// }
+enum Menu { google_sign_in, firestore_cloud_vision }
+
+
 class MaimPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -31,32 +44,147 @@ class MaimPage extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: Text("TODO アプリだよね"),
+          actions: <Widget>[
+            Consumer<MainModel>(builder: (context, model, child) {
+              final isActive = model.checkShouldActiveCompleteButton();
+              return FlatButton(
+                onPressed: isActive
+                    ? () async {
+                        model.deleteCheckedItems();
+                      }
+                    : null,
+                child: Text(
+                  "完了",
+                  style: TextStyle(
+                    // 色を白にしている
+                    color: isActive
+                        ? Colors.white
+                        : Colors.white.withOpacity(0.5), // 透明度
+                  ),
+                ),
+              );
+            }),
+          ],
         ),
         body: Consumer<MainModel>(
           builder: (context, model, child) {
             final todoList = model.todoList;
             return ListView(
               children: todoList
-                  .map((todo) => ListTile(
-                        title: Text(todo.title),
-                      ))
+                  .map(
+                    (todo) => _todoItem(
+                      // チェックボックを追加
+                      todo.title,
+                      Icon(Icons.more_vert),
+                    ),
+                  )
                   .toList(),
             );
           },
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            await Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => AddPage(),
-                fullscreenDialog: true, // 画面遷移のアニメーション
-              ),
-            );
-          },
-          child: Icon(Icons.add),
-        ),
+        floatingActionButton:
+            Consumer<MainModel>(builder: (context, model, child) {
+          return FloatingActionButton(
+            onPressed: () async {
+              // 画面遷移
+              // await Navigator.push(1
+              //   context,
+              //   MaterialPageRoute(
+              //     builder: (context) => AddPage(),
+              //     fullscreenDialog: true, // 画面遷移のアニメーション
+              //   ),
+              // );
+
+              // ダイアログ
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SimpleDialog(
+                      title: Text("Todoの追加"),
+                      children: <Widget>[
+                        // コンテンツ領域
+                        StreamBuilder<Object>(
+                            stream: null,
+                            builder: (context, snapshot) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    labelText: "追加するTODO",
+                                    hintText: "例：ゴミを出す",
+                                  ),
+                                  onChanged: (text) {
+                                    // 入力したテキストを取得してモデルに代入
+                                    model.todoText = text;
+                                  },
+                                ),
+                              );
+                            }),
+                        Row(
+                          children: [
+                            FlatButton(
+                              onPressed: () {
+                                model.add();
+                                Navigator.pop(context);
+                              },
+                              child: Text("キャンセル"),
+                            ),
+                            FlatButton(
+                              onPressed: () {
+                                model.add();
+                                Navigator.pop(context);
+                              },
+                              child: Text("追加"),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+            child: Icon(Icons.add),
+          );
+        }),
       ),
+    );
+  }
+
+  Widget _todoItem(String title, Icon icon) {
+    return GestureDetector(
+      child: Container(
+          padding: EdgeInsets.all(8.0),
+          decoration: new BoxDecoration(
+              border: new Border(
+                  bottom: BorderSide(width: 1.0, color: Colors.grey))),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween, // 両端に寄せる
+            children: <Widget>[
+              Text(
+                title,
+                style: TextStyle(color: Colors.black, fontSize: 18.0),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: icon,
+              ),
+              PopupMenuButton(
+                itemBuilder: (BuildContext context) =>
+                <PopupMenuEntry<Menu>>[
+                  const PopupMenuItem( child: const ListTile( leading:Icon(Icons.supervisor_account),
+                      title:Text("Google Sign In")), value: Menu.google_sign_in),
+                  const PopupMenuItem( child: const ListTile( leading:Icon(Icons.crop_original),
+                      title:Text("Firestore,CloudVision")), value: Menu.firestore_cloud_vision,),
+                ],
+              ),
+            ],
+          )),
+      onTap: () {
+        print("onTap called.");
+      },
     );
   }
 }
--- myapps/flutter/flutter_todo_app_firebase/lib/main_model.dart
+++ myapps/flutter/flutter_todo_app_firebase/lib/main_model.dart
@@ -4,10 +4,11 @@ import 'package:flutter_todo_app_firebase/todo.dart';
 
 class MainModel extends ChangeNotifier {
   List<Todo> todoList = [];
+  String todoText = '';
 
-  Future getTodoList() async {
+  Future getTodoList() async { // Futureは非同期処理
     final snapshot =
-    await FirebaseFirestore.instance.collection("todoList").get();
+        await FirebaseFirestore.instance.collection("todoList").get();
     final docs = snapshot.docs;
     final todoList = docs.map((doc) => Todo(doc)).toList();
     this.todoList = todoList;
@@ -15,14 +16,45 @@ class MainModel extends ChangeNotifier {
   }
 
   void getTodoListRealtime() {
-    final snapshot = FirebaseFirestore.instance.collection("todoList")
-        .snapshots();
+    final snapshot =
+        FirebaseFirestore.instance.collection("todoList").snapshots();
     snapshot.listen((snapshot) {
       final docs = snapshot.docs;
       final todoList = docs.map((doc) => Todo(doc)).toList();
       todoList.sort((a, b) => b.createdAt.compareTo(a.createdAt));
       this.todoList = todoList;
       notifyListeners();
+    });
+  }
+
+  void reload() {
+    notifyListeners();
+  }
+
+  void deleteCheckedItems() async {
+    final checkedItems = todoList.where((todo) => todo.isDone).toList(); // whereはtrueだけ残るので、
+    final references =
+        checkedItems.map((todo) => todo.documentReference).toList(); // ドキュメントリファレンスのlistに変換
+
+    final batch = FirebaseFirestore.instance.batch();
+
+    references.forEach((reference) {
+      batch.delete(reference);
+    });
+    return batch.commit();
+  }
+
+  bool checkShouldActiveCompleteButton() {
+    final checkedItems = todoList.where((todo) => todo.isDone).toList();
+    return checkedItems.length > 0;
+  }
+
+  Future add() async{
+    final collection = FirebaseFirestore.instance.collection("todoList");
+    // 追加のドキュメント → https://www.youtube.com/watch?v=rR0Zl9-J0JA&list=PLuLRJz1UnJzEQDOEflFsTiWFFgoLQqlK0&index=3&t=406s
+    await collection.add({
+      'title': todoText,
+      'createdAt': Timestamp.now(),
     });
   }
 }
